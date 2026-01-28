42de5d13fdccc53189a75759297f6092524ba70f
Stiven <Strnmrtnz@icloud.com>
2026-01-28 21:02:30 +0530

    fix: some hyperparameter changes after getting a inliers count

--- wip/core/mapper_tuned.py
+++ wip/core/mapper_tuned.py
@@ -3,7 +3,17 @@ Reconstruction step using COLMAP mapper
 
 Assuming a well connected image graph thanks to query expansion, it uses a normal mapper with fairly default settings. 
 
-If reconstructions fail to converge, it'd be worth trying a hierarchical_mapper instead. 
+If reconstructions fail to converge, it'd be worth trying a hierarchical_mapper instead.
+
+Note, as of Jan 28, this is the distribution of matches:
+
+100-199 inliers|258498
+15-29 inliers|5278
+200-299 inliers|59541
+30-49 inliers|139294
+300+ inliers|720847
+50-99 inliers|675462
+<15 inliers|4673433
 
 """
 
@@ -60,12 +70,13 @@ def reconstruction(database_path, image_path, output_path, image_list_path) -> N
         "--Mapper.ba_gpu_index", "-1",                 # Auto-detect GPU
         "--Mapper.num_threads", "-1",                  # Auto-detect threads
         "--Mapper.ignore_watermarks", "1",             # Ignore Watermarks
-        "--Mapper.init_min_num_inliers", "200",        # Most samples have 30 > inliers. This should constraint the starting pairs better. 
-        "--Mapper.init_max_forward_motion", "0.4",     # Should be stricter about having the initial pairs be separate from each other, rather than, for example, a picture of two very-similar looking houses facing forwards (this should have it see the features are different thanks to the angular separation)
-        "--Mapper.init_num_trials", "400",             # I saw many initial matches fail a lot, and even at 200 min_inliers, there are plenty, so it's good to try lots for rebudancy 
+        "--Mapper.init_min_num_inliers", "300",        # Most samples have 30+ inliers. This should constrain the starting pairs to the 10% (700k+) pairs with 300+ inliers. 
+        "--Mapper.init_max_forward_motion", "0.3",     # Should be stricter about having the initial pairs be separate from each other, rather than, for example, a picture of two very-similar looking houses facing forwards (this should have it see the features are different thanks to the angular separation)
+        "--Mapper.init_num_trials", "400",             # I saw many initial matches fail a lot, and even at 200 min_inliers there are plenty, so it's good to try lots for redundancy 
         "--Mapper.init_min_tri_angle", "32",           # Twice as much as the default for stricter geometry matches
         "--Mapper.abs_pose_min_inlier_ratio", "0.40",  # Almost twice as much as the default. Should significantly cut down on false positives and matches that aren't strong.
-        "--Mapper.abs_pose_min_num_inliers", "80",     # Too many images are 30 > inliers, so this should cut down on easy matches and barely related images    ]
+        "--Mapper.abs_pose_min_num_inliers", "80",     # Too many images are 30+ inliers, so this should cut down on easy matches and barely related images
+    ]
 
     with open(log_file_path, 'w') as log_file:
         try:
